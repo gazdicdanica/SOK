2a33dc116e5a41536b17c8db9092ea8ecd643412
sv25/2020 <magazin.sv25.2020@uns.ac.rs>
2022-12-21 23:28:20 +0100

    #1 chore: Create core app within the Django project

--- kenigsberg/kenigsberg/settings.py
+++ kenigsberg/kenigsberg/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
