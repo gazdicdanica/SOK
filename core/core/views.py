92830ad9b6d2820cb4714ad6a43b66991fa10dab
sv25/2020 <magazin.sv25.2020@uns.ac.rs>
2022-12-23 11:18:51 +0100

    #2 feat: Implement graph data model

--- core/core/views.py
+++ core/core/views.py
@@ -3,6 +3,8 @@ from django.apps.registry import apps
 from django.shortcuts import render
 
 from .services.parse import DataParserBase
+from .services.model import Node, Edge, Graph
+
 
 def index(request):
     parse_plugins: List[DataParserBase] = apps.get_app_config('core').parse_plugins
@@ -10,4 +12,32 @@ def index(request):
     print("Parse Plugins: " + ", ".join(plugin.name() for plugin in parse_plugins))
     print("Render Plugins: " + ", ".join(plugin.name() for plugin in render_plugins))
 
+    test_graph()
+
     return render(request, "hello_world.html")
+
+
+def test_graph():
+    n1 = Node()
+    n2 = Node()
+    n3 = Node()
+    n4 = Node()
+
+    e1 = Edge(n1, n2)
+    e2 = Edge(n1, n3)
+    e3 = Edge(n2, n3)
+    e4 = Edge(n4, n1)
+    e5 = Edge(n4, n3)
+
+    nodes = [n1, n2, n3, n4]
+    edges = [e1, e2, e3, e4, e5]
+
+    g = Graph(nodes, edges)
+
+    print(len(g.nodes))
+    for n in g.nodes:
+        print(n)
+
+    print(len(g.edges))
+    for e in g.edges:
+        print(e)
