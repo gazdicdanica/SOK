31e7fb4f157da1270652c297233d945c2d380e7a
unknown <isomidobradovic@gmail.com>
2022-12-25 16:37:45 +0100

    #5: Started work on JSON-source-plugin

--- parse_json/core/services/parse_json.py
+++ parse_json/core/services/parse_json.py
@@ -1,20 +1,44 @@
+import json
+
 from core.services.parse import DataParserBase
+
+from core.core.services.model import *
+
+
+def loadData(fname: str):
+    print(fname)
+    with open(fname, 'rt', buffering=100000) as fn:
+        data = fn.read()
+    return json.loads(data)
 
 
 class JSONParser(DataParserBase):
     def identifier(self) -> str:
         return "#JSONParser"
 
-
     def name(self) -> str:
         return "JSONParser"
-
 
     def parse(self, data: str) -> None:
         print(data)
         return None
 
-
     def parseFile(self, fname: str) -> None:
-        print(fname)
-        return None
+        fname = "../../test_data/data.json"
+        return self.createGraph(loadData(fname))
+
+    def createGraph(self, parsed_data):
+        # return self.connectGraph(parsed_data, Graph())
+        return self.connectGraph(parsed_data)
+
+    # def connectGraph(self, current_dict, graph, parent=None):
+    def connectGraph(self, current_dict, parent=None):
+        v = Node()
+        for key in current_dict.values():
+            print(key)
+
+
+if __name__ == '__main__':
+    parse_json = JSONParser()
+    parse_json.parseFile("")
+
