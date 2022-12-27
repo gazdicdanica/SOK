a54e275832c86d21e7b6c0326ee94cc73d0d7f6d
Danica Gazdić <danicagazdic@gmail.com>
2022-12-27 10:33:04 +0100

    "#6 Implementation of YAML parser without special cases ex. reference attribute in file"

--- parse_yaml/core/services/parse_yaml.py
+++ parse_yaml/core/services/parse_yaml.py
@@ -1,4 +1,6 @@
+import yaml
 from core.services.parse import DataParserBase
+from core.services.model import *
 
 
 class YAMLParser(DataParserBase):
@@ -11,10 +13,43 @@ class YAMLParser(DataParserBase):
 
 
     def parse(self, data: str) -> None:
-        print(data)
         return None
 
 
-    def parseFile(self, fname: str) -> None:
+    def parseFile(self, fname: str) -> Dict:
         print(fname)
-        return None
+        with open(fname, 'r') as file:
+            dataMap = yaml.safe_load(file)
+        return dataMap
+
+    def processParsedData(self, data, parent_node: Node, graph: Graph):
+        node = Node()
+        for key in data:
+            if type(data[key]) is list:
+                if(all(type(n) is dict for n in data[key])):
+                    for child_node in data[key]:
+                        self.processParsedData(child_node, node, graph)
+                else:
+                    node.attr[key] = data[key]
+            elif type(data[key]) is dict:
+                self.processParsedData(data[key], node, graph)
+            else:
+                node.attr[key] = data[key]
+
+        self.connectNodes(graph, node, parent_node)
+
+
+    def connectNodes(self, graph: Graph, child_node: Node, parent_node: Node):
+        if parent_node:
+            edge = Edge(parent_node, child_node)
+            graph.edges.append(edge)
+
+        graph.nodes.append(child_node)
+
+
+    def generateGraph(self) -> Graph:
+        graph = Graph([], [])
+
+        data = self.parseFile("./data/test.yml")
+
+        self.processParsedData(data, None, graph)
